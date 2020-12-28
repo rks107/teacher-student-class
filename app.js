b0bdb5400cfa6f5b3b945558fe1eb9c4439b13cb
ROHIT KUMAR <sumitkumar@rohit-2.local>
2020-12-28 15:38:47 +0530

    Add home controller

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
+// routers
+app.use('/', require('./routes'));
+
 app.listen(PORT, function(err){
     if(err){
         console.log(`Error in connecting server ${err}`);
